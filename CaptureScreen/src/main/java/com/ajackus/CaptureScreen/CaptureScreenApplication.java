0030c173f02ad44deb91451ea83ccbf044910b16
deepak <deepakpareek1@gmail.com>
2020-10-24 19:47:52 +0530

    Application scaffolding

---
+++ CaptureScreen/src/main/java/com/ajackus/CaptureScreen/CaptureScreenApplication.java
@@ -0,0 +1,13 @@
+package com.ajackus.CaptureScreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureScreenApplication.class, args);
+	}
+
+}
---
+++ CaptureScreen/src/test/java/com/ajackus/CaptureScreen/CaptureScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajackus.CaptureScreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
